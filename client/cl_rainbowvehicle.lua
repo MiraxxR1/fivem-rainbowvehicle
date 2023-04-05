fbc89badfd348d8199bf9152c20321a2d3abed0d
MiraxxR <75571297+MiraxxR1@users.noreply.github.com>
2023-04-05 21:28:16 +0200

    Add files via upload

---
+++ client/cl_rainbowvehicle.lua
@@ -0,0 +1,37 @@
+local currentVehicle = nil
+
+local function getRandomColor()
+    local _tbl = {}
+    _tbl.r = math.random(0, 255)
+    _tbl.g = math.random(0, 255)
+    _tbl.b = math.random(0, 255)
+    return _tbl
+end
+
+local function SetVehicleColour()
+    local playerPed <const> = PlayerPedId()
+    currentVehicle = GetVehiclePedIsIn(playerPed, false)
+    ToggleVehicleMod(currentVehicle, 22, true)
+    SetVehicleNeonLightEnabled(currentVehicle, 0, true)
+	SetVehicleNeonLightEnabled(currentVehicle, 1, true)
+	SetVehicleNeonLightEnabled(currentVehicle, 2, true)
+	SetVehicleNeonLightEnabled(currentVehicle, 3, true)
+    if currentVehicle ~= nil and DoesEntityExist(currentVehicle) then
+        local _vehicle_color = getRandomColor()
+        local _neon_color = getRandomColor()
+        SetVehicleCustomPrimaryColour(currentVehicle, _vehicle_color.r, _vehicle_color.g, _vehicle_color.b)
+        SetVehicleCustomSecondaryColour(currentVehicle, _vehicle_color.r, _vehicle_color.g, _vehicle_color.b)
+        SetVehicleNeonLightsColour(currentVehicle, _neon_color.r, _neon_color.g, _neon_color.b)
+        SetVehicleHeadlightsColour(currentVehicle, math.random(0, 12))
+        SetVehicleNumberPlateText(currentVehicle, 'MiraxxR')
+    end
+end
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(500)
+        SetVehicleColour()
+    end
+end)
+
+print("^1 fivem-rainbowvehicle - By MiraxxR#8801 - https://github.com/MiraxxR1/fivem-rainbowvehicle - https://discord.gg/wJmnKQHTWD^7")
---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+name 'fivem-rainbowvehicle'
+author 'MiraxxR'
+contact 'MiraxxR#8801'
+repository 'https://github.com/MiraxxR1/fivem-rainbowvehicle'
+discord 'https://discord.gg/wJmnKQHTWD'
+description 'This script for FiveM allows your players to give a dynamic effect to your vehicle by changing the color, neon lights and headlights every second on your server.'
+
+client_scripts {
+    "client/cl_rainbowvehicle.lua"
+}
